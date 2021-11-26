c075ac0c9b4a689ce1d8d745bfa6d07c7486844d
MoonTM <benzweitacc@gmail.com>
2021-11-26 18:50:19 +0100

    Added Javadoc to SlashCommandInteraction.java

--- src/main/java/com/dynxsty/dih4jda/slash_command/SlashCommandInteraction.java
+++ src/main/java/com/dynxsty/dih4jda/slash_command/SlashCommandInteraction.java
@@ -2,5 +2,9 @@ package com.dynxsty.dih4jda.slash_command;
 
 import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
 
+/**
+ * @param handler The {@link ISlashCommand}.
+ * @param privileges A array of {@link CommandPrivilege}'s
+ */
 public record SlashCommandInteraction(ISlashCommand handler, CommandPrivilege[] privileges) {
 }
