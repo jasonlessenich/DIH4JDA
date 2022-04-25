b19a5261f397bfd036ad71ce325fc790752f7d43
Dynxsty <jasonlessenich@gmail.com>
2022-04-25 23:09:17 +0200

    Introduction of `AutoCompleteUtils.java`

---
+++ src/main/java/com/dynxsty/dih4jda/util/AutoCompleteUtils.java
@@ -0,0 +1,37 @@
+package com.dynxsty.dih4jda.util;
+
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.Command;
+
+import java.util.List;
+
+/**
+ * Utility class that contains some useful methods regarding the AutoComplete system.
+ */
+public class AutoCompleteUtils {
+
+	private AutoCompleteUtils() {}
+
+	/**
+	 * Filters all AutoComplete choices based on the user's current input.
+	 *
+	 * @param event The {@link CommandAutoCompleteInteractionEvent} that was fired.
+	 * @param choices A {@link List} of {@link Command.Choice}s.
+	 * @return The filtered {@link List} of {@link Command.Choice}s.
+	 */
+	public static List<Command.Choice> filterChoices(CommandAutoCompleteInteractionEvent event, List<Command.Choice> choices) {
+		return AutoCompleteUtils.filterChoices(event.getFocusedOption().getValue().toLowerCase(), choices);
+	}
+
+	/**
+	 * Filters all AutoComplete choices based on the user's current input.
+	 *
+	 * @param filter The filter.
+	 * @param choices A {@link List} of {@link Command.Choice}s.
+	 * @return The filtered {@link List} of {@link Command.Choice}s.
+	 */
+	public static List<Command.Choice> filterChoices(String filter, List<Command.Choice> choices) {
+		choices.removeIf(choice -> !choice.getName().toLowerCase().contains(filter.toLowerCase()));
+		return choices;
+	}
+}
