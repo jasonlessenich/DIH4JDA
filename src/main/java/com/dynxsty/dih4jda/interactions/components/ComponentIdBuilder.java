fcd983311840552c8fb845004458c7ccfa075c89
Dynxsty <jasonlessenich@gmail.com>
2022-04-25 22:39:47 +0200

    added some missing javadoc

--- src/main/java/com/dynxsty/dih4jda/interactions/components/ComponentIdBuilder.java
+++ src/main/java/com/dynxsty/dih4jda/interactions/components/ComponentIdBuilder.java
@@ -3,18 +3,37 @@ package com.dynxsty.dih4jda.interactions.components;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+/**
+ * Utility class that allows for easy and consistent component-id building.
+ */
 public class ComponentIdBuilder {
-
 	private static String separator = ":";
 
+	/**
+	 * Changes the default component-id separator.
+	 *
+	 * @param separator The string that should act as the separator.
+	 */
 	public static void setDefaultSeparator(String separator) {
 		ComponentIdBuilder.separator = separator;
 	}
 
+	/**
+	 * Gets the current component-id separator.
+	 *
+	 * @return The current component-id separator.
+	 */
 	public static String getSeparator() {
 		return separator;
 	}
 
+	/**
+	 * Builds a component-id using the (set) {@link ComponentIdBuilder#separator} and the given identifier and arguments.
+	 *
+	 * @param identifier The component's identifier.
+	 * @param args An optional parameter for arguments.
+	 * @return The built component-id, as a {@link String}.
+	 */
 	public static String build(String identifier, Object... args) {
 		return identifier + separator + Arrays.stream(args).map(Object::toString).collect(Collectors.joining(separator));
 	}
