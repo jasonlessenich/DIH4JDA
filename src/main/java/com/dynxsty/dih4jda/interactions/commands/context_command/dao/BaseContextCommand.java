6a0f142d34fe3bcd774b8f3dfad03b529adce525
Dynxsty <jasonlessenich@gmail.com>
2022-04-27 23:57:33 +0200

    removed deprecated interaction handler methods from `BaseContextCommand`

--- src/main/java/com/dynxsty/dih4jda/interactions/commands/context_command/dao/BaseContextCommand.java
+++ src/main/java/com/dynxsty/dih4jda/interactions/commands/context_command/dao/BaseContextCommand.java
@@ -4,13 +4,8 @@ import com.dynxsty.dih4jda.DIH4JDALogger;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
-import java.util.List;
-
 public abstract class BaseContextCommand {
 	private CommandData commandData;
-	private List<String> handledButtonIds;
-	private List<String> handledSelectMenuIds;
-	private List<String> handledModalIds;
 
 	protected BaseContextCommand() {
 	}
@@ -25,29 +20,5 @@ public abstract class BaseContextCommand {
 		} else {
 			DIH4JDALogger.error(String.format("Invalid Command Type \"%s\" for Context Command! This command will be ignored.", commandData.getType()));
 		}
-	}
-
-	public List<String> getHandledButtonIds() {
-		return handledButtonIds;
-	}
-
-	public void setHandledButtonIds(List<String> handledButtonIds) {
-		this.handledButtonIds = handledButtonIds;
-	}
-
-	public List<String> getHandledSelectMenuIds() {
-		return handledSelectMenuIds;
-	}
-
-	public void setHandledSelectMenuIds(List<String> handledSelectMenuIds) {
-		this.handledSelectMenuIds = handledSelectMenuIds;
-	}
-
-	public List<String> getHandledModalIds() {
-		return handledModalIds;
-	}
-
-	public void handleModalIds(List<String> handledModalIds) {
-		this.handledModalIds = handledModalIds;
 	}
 }
