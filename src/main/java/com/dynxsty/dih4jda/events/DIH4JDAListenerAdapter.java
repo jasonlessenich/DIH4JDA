fdb5f4e86ef783a83057a3787156eff8ae1213c0
Dynxsty <jasonlessenich@gmail.com>
2022-05-08 15:55:43 +0200

    Started work on implementing Events

---
+++ src/main/java/com/dynxsty/dih4jda/events/DIH4JDAListenerAdapter.java
@@ -0,0 +1,16 @@
+package com.dynxsty.dih4jda.events;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
+import net.dv8tion.jda.api.interactions.components.ComponentInteraction;
+
+import java.util.Set;
+
+public abstract class DIH4JDAListenerAdapter {
+
+    // Exception
+    public void onCommandException(CommandInteraction interaction, Exception e) {}
+    public void onComponentException(ComponentInteraction interaction, Exception e) {}
+
+    public void onInsufficientPermission(CommandInteraction interaction, Set<Permission> permissions) {}
+}
