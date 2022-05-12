e265e4f5eeea440493c493f3cb9a48a1000f7899
Dynxsty <jasonlessenich@gmail.com>
2022-05-12 07:35:51 +0200

    some cleanup

--- src/main/java/com/dynxsty/dih4jda/DIH4JDA.java
+++ src/main/java/com/dynxsty/dih4jda/DIH4JDA.java
@@ -29,139 +29,139 @@ import java.util.stream.Collectors;
  */
 public class DIH4JDA extends ListenerAdapter {
 
-	public static ExecutableCommand.Type defaultCommandType;
-	private final JDA jda;
-	private final String commandsPackage;
-	private final Set<DIH4JDALogger.Type> blockedLogTypes;
-	private final boolean registerOnReady;
-	private final boolean smartQueuing;
-	private final boolean deleteUnknownCommands;
-	private final Set<DIH4JDAListenerAdapter> listeners;
-	private final Executor executor;
-	private final InteractionHandler handler;
+    public static ExecutableCommand.Type defaultCommandType;
+    private final JDA jda;
+    private final String commandsPackage;
+    private final Set<DIH4JDALogger.Type> blockedLogTypes;
+    private final boolean registerOnReady;
+    private final boolean smartQueuing;
+    private final boolean deleteUnknownCommands;
+    private final Set<DIH4JDAListenerAdapter> listeners;
+    private final Executor executor;
+    private final InteractionHandler handler;
 
-	/**
-	 * Constructs a new DIH4JDA instance
-	 *
-	 * @param jda             The {@link JDA} instance the handler is to be used for.
-	 * @param commandsPackage The package that houses the command classes.
-	 * @param blockedLogTypes All Logs that should be blocked.
-	 */
-	protected DIH4JDA(JDA jda, String commandsPackage, boolean registerOnReady, boolean smartQueuing, boolean deleteUnknownCommands, Executor executor, DIH4JDALogger.Type... blockedLogTypes) {
-		this.jda = jda;
-		this.commandsPackage = commandsPackage;
-		this.registerOnReady = registerOnReady;
-		this.smartQueuing = smartQueuing;
-		this.deleteUnknownCommands = deleteUnknownCommands;
-		if (blockedLogTypes == null || blockedLogTypes.length < 1) {
-			this.blockedLogTypes = new HashSet<>();
-		} else {
-			this.blockedLogTypes = Arrays.stream(blockedLogTypes).collect(Collectors.toSet());
-		}
-		this.executor = executor;
-		this.listeners = new HashSet<>();
-		this.handler = new InteractionHandler(this);
-		if (defaultCommandType == null) defaultCommandType = ExecutableCommand.Type.GUILD;
-		jda.addEventListener(this, handler);
-	}
+    /**
+     * Constructs a new DIH4JDA instance
+     *
+     * @param jda             The {@link JDA} instance the handler is to be used for.
+     * @param commandsPackage The package that houses the command classes.
+     * @param blockedLogTypes All Logs that should be blocked.
+     */
+    protected DIH4JDA(JDA jda, String commandsPackage, boolean registerOnReady, boolean smartQueuing, boolean deleteUnknownCommands, Executor executor, DIH4JDALogger.Type... blockedLogTypes) {
+        this.jda = jda;
+        this.commandsPackage = commandsPackage;
+        this.registerOnReady = registerOnReady;
+        this.smartQueuing = smartQueuing;
+        this.deleteUnknownCommands = deleteUnknownCommands;
+        if (blockedLogTypes == null || blockedLogTypes.length < 1) {
+            this.blockedLogTypes = new HashSet<>();
+        } else {
+            this.blockedLogTypes = Arrays.stream(blockedLogTypes).collect(Collectors.toSet());
+        }
+        this.executor = executor;
+        this.listeners = new HashSet<>();
+        this.handler = new InteractionHandler(this);
+        if (defaultCommandType == null) defaultCommandType = ExecutableCommand.Type.GUILD;
+        jda.addEventListener(this, handler);
+    }
 
-	/**
-	 * Ran once the {@link JDA} instance fires the {@link ReadyEvent}.
-	 *
-	 * @param event The {@link ReadyEvent} that was fired.
-	 */
-	@Override
-	public void onReady(@NotNull ReadyEvent event) {
-		if (getCommandsPackage() == null) return;
-		DIH4JDALogger.blockedLogTypes = blockedLogTypes;
-		try {
-			if (registerOnReady && handler != null) {
-				handler.registerInteractions();
-			}
-		} catch (ReflectiveOperationException e) {
-			DIH4JDALogger.error("Could not register commands: " + e.getMessage());
-		}
-	}
+    /**
+     * Ran once the {@link JDA} instance fires the {@link ReadyEvent}.
+     *
+     * @param event The {@link ReadyEvent} that was fired.
+     */
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        if (getCommandsPackage() == null) return;
+        DIH4JDALogger.blockedLogTypes = blockedLogTypes;
+        try {
+            if (registerOnReady && handler != null) {
+                handler.registerInteractions();
+            }
+        } catch (ReflectiveOperationException e) {
+            DIH4JDALogger.error("Could not register commands: " + e.getMessage());
+        }
+    }
 
-	/**
-	 * Registers all Interactions and replaces the old ones.
-	 * Please note that global commands may need up to an hour before they're fully registered.
-	 */
-	public void registerInteractions() throws ReflectiveOperationException {
-		handler.registerInteractions();
-	}
+    /**
+     * Registers all Interactions and replaces the old ones.
+     * Please note that global commands may need up to an hour before they're fully registered.
+     */
+    public void registerInteractions() throws ReflectiveOperationException {
+        handler.registerInteractions();
+    }
 
-	/**
-	 * @return The {@link JDA} instance.
-	 */
-	public JDA getJDA() {
-		return jda;
-	}
+    /**
+     * @return The {@link JDA} instance.
+     */
+    public JDA getJDA() {
+        return jda;
+    }
 
-	/**
-	 * @return The provided package that is used with the {@link Reflections} API.
-	 */
-	public String getCommandsPackage() {
-		return commandsPackage;
-	}
+    /**
+     * @return The provided package that is used with the {@link Reflections} API.
+     */
+    public String getCommandsPackage() {
+        return commandsPackage;
+    }
 
-	/**
-	 * @return The executor that is used to execute the commands.
-	 */
-	public Executor getExecutor() {
-		return executor;
-	}
+    /**
+     * @return The executor that is used to execute the commands.
+     */
+    public Executor getExecutor() {
+        return executor;
+    }
 
-	/**
-	 * @return A set with all blocked {@link DIH4JDALogger.Type}s.
-	 */
-	public Set<DIH4JDALogger.Type> getBlockedLogTypes() {
-		return blockedLogTypes;
-	}
+    /**
+     * @return A set with all blocked {@link DIH4JDALogger.Type}s.
+     */
+    public Set<DIH4JDALogger.Type> getBlockedLogTypes() {
+        return blockedLogTypes;
+    }
 
-	/**
-	 * @return Whether commands should be registered on each {@link ListenerAdapter#onReady(ReadyEvent)} event.
-	 */
-	public boolean isRegisterOnReady() {
-		return registerOnReady;
-	}
+    /**
+     * @return Whether commands should be registered on each {@link ListenerAdapter#onReady(ReadyEvent)} event.
+     */
+    public boolean isRegisterOnReady() {
+        return registerOnReady;
+    }
 
-	/**
-	 * @return Whether the SmartQueue functionality is enabled.
-	 */
-	public boolean isSmartQueuing() {
-		return smartQueuing;
-	}
+    /**
+     * @return Whether the SmartQueue functionality is enabled.
+     */
+    public boolean isSmartQueuing() {
+        return smartQueuing;
+    }
 
-	/**
-	 * @return Whether the SmartQueueDeleteUnknown functionality is enabled.
-	 */
-	public boolean isDeletingUnknownCommands() {
-		return deleteUnknownCommands;
-	}
+    /**
+     * @return Whether the SmartQueueDeleteUnknown functionality is enabled.
+     */
+    public boolean isDeletingUnknownCommands() {
+        return deleteUnknownCommands;
+    }
 
-	/**
-	 * Allows to add Listener classes (that extend {@link DIH4JDAListenerAdapter}).
-	 *
-	 * @since v1.5
-	 */
-	public void addListeners(Object... classes) {
-		for (Object o : classes) {
-			try {
-				// check if class extends the ListenerAdapter
-				DIH4JDAListenerAdapter adapter = (DIH4JDAListenerAdapter) o;
-				listeners.add(adapter);
-			} catch (ClassCastException e) {
-				throw new IllegalArgumentException("Listener classes must extend DIH4JDAListenerAdapter!");
-			}
-		}
-	}
+    /**
+     * Allows to add Listener classes (that extend {@link DIH4JDAListenerAdapter}).
+     *
+     * @since v1.5
+     */
+    public void addListeners(Object... classes) {
+        for (Object o : classes) {
+            try {
+                // check if class extends the ListenerAdapter
+                DIH4JDAListenerAdapter adapter = (DIH4JDAListenerAdapter) o;
+                listeners.add(adapter);
+            } catch (ClassCastException e) {
+                throw new IllegalArgumentException("Listener classes must extend DIH4JDAListenerAdapter!");
+            }
+        }
+    }
 
-	/**
-	 * @return A set of all Listener classes.
-	 * @see DIH4JDA#addListeners(Object...)
-	 */
-	protected Set<DIH4JDAListenerAdapter> getListeners() {
-		return listeners;
-	}
+    /**
+     * @return A set of all Listener classes.
+     * @see DIH4JDA#addListeners(Object...)
+     */
+    protected Set<DIH4JDAListenerAdapter> getListeners() {
+        return listeners;
+    }
 }
--- src/main/java/com/dynxsty/dih4jda/DIH4JDABuilder.java
+++ src/main/java/com/dynxsty/dih4jda/DIH4JDABuilder.java
@@ -14,120 +14,120 @@ import java.util.concurrent.ForkJoinPool;
  * Builder-System used to build {@link DIH4JDA}.
  */
 public class DIH4JDABuilder {
-	private final JDA jda;
-	private String commandsPackage;
-	private Executor executor = ForkJoinPool.commonPool();
-	private DIH4JDALogger.Type[] blockedLogTypes;
-	private boolean registerOnStartup = true;
-	private boolean smartQueuing = true;
-	private boolean deleteUnknownCommands = true;
+    private final JDA jda;
+    private String commandsPackage;
+    private Executor executor = ForkJoinPool.commonPool();
+    private DIH4JDALogger.Type[] blockedLogTypes;
+    private boolean registerOnStartup = true;
+    private boolean smartQueuing = true;
+    private boolean deleteUnknownCommands = true;
 
-	private DIH4JDABuilder(@Nonnull JDA jda) {
-		this.jda = jda;
-	}
+    private DIH4JDABuilder(@Nonnull JDA jda) {
+        this.jda = jda;
+    }
 
-	/**
-	 * Sets the {@link JDA} instance the handler will be used for.
-	 *
-	 * @param instance The {@link JDA} instance.
-	 */
-	public static DIH4JDABuilder setJDA(JDA instance) {
-		return new DIH4JDABuilder(instance);
-	}
+    /**
+     * Sets the {@link JDA} instance the handler will be used for.
+     *
+     * @param instance The {@link JDA} instance.
+     */
+    public static DIH4JDABuilder setJDA(JDA instance) {
+        return new DIH4JDABuilder(instance);
+    }
 
-	/**
-	 * Sets the package that houses all Command classes. DIH4JDA then uses the {@link org.reflections.Reflections} API to "scan" the package for all
-	 * command classes.
-	 *
-	 * @param pack The package's name.
-	 */
-	@Nonnull
-	public DIH4JDABuilder setCommandsPackage(@Nonnull String pack) {
-		commandsPackage = pack;
-		return this;
-	}
+    /**
+     * Sets the package that houses all Command classes. DIH4JDA then uses the {@link org.reflections.Reflections} API to "scan" the package for all
+     * command classes.
+     *
+     * @param pack The package's name.
+     */
+    @Nonnull
+    public DIH4JDABuilder setCommandsPackage(@Nonnull String pack) {
+        commandsPackage = pack;
+        return this;
+    }
 
-	/**
-	 * Sets the Executor that will be used to execute all commands.
-	 *
-	 * @param executor The Executor.
-	 */
-	@Nonnull
-	public DIH4JDABuilder setExecutor(@Nonnull Executor executor) {
-		this.executor = executor;
-		return this;
-	}
+    /**
+     * Sets the Executor that will be used to execute all commands.
+     *
+     * @param executor The Executor.
+     */
+    @Nonnull
+    public DIH4JDABuilder setExecutor(@Nonnull Executor executor) {
+        this.executor = executor;
+        return this;
+    }
 
-	/**
-	 * Sets the types of logging that should be disabled.
-	 *
-	 * @param types All {@link DIH4JDALogger.Type}'s that should be disabled.
-	 */
-	@Nonnull
-	public DIH4JDABuilder disableLogging(DIH4JDALogger.Type... types) {
-		if (types == null || types.length < 1) {
-			blockedLogTypes = DIH4JDALogger.Type.values();
-		} else {
-			blockedLogTypes = types;
-		}
-		return this;
-	}
+    /**
+     * Sets the types of logging that should be disabled.
+     *
+     * @param types All {@link DIH4JDALogger.Type}'s that should be disabled.
+     */
+    @Nonnull
+    public DIH4JDABuilder disableLogging(DIH4JDALogger.Type... types) {
+        if (types == null || types.length < 1) {
+            blockedLogTypes = DIH4JDALogger.Type.values();
+        } else {
+            blockedLogTypes = types;
+        }
+        return this;
+    }
 
-	/**
-	 * Whether DIH4JDA should automatically register all interactions on Startup.
-	 * A manual registration of all interactions can be executed using {@link DIH4JDA#registerInteractions()}.
-	 */
-	@Nonnull
-	public DIH4JDABuilder disableAutomaticCommandRegistration() {
-		registerOnStartup = false;
-		return this;
-	}
+    /**
+     * Whether DIH4JDA should automatically register all interactions on Startup.
+     * A manual registration of all interactions can be executed using {@link DIH4JDA#registerInteractions()}.
+     */
+    @Nonnull
+    public DIH4JDABuilder disableAutomaticCommandRegistration() {
+        registerOnStartup = false;
+        return this;
+    }
 
-	/**
-	 * <b>NOT RECOMMENDED</b> (unless there are some bugs) <br>
-	 * This will disable the Smart Queueing functionality.
-	 * If SmartQueue is disabled Global Slash/Context Commands get overridden on each {@link DIH4JDA#registerInteractions()} call,
-	 * thus, making Global Commands unusable for about an hour, until they're registered again. <br>
-	 * By default, this also deletes unknown/unused commands. This behaviour can be disabled with {@link DIH4JDABuilder#disableUnknownCommandDeletion()}.
-	 */
-	@Nonnull
-	public DIH4JDABuilder disableSmartQueue() {
-		smartQueuing = false;
-		return this;
-	}
+    /**
+     * <b>NOT RECOMMENDED</b> (unless there are some bugs) <br>
+     * This will disable the Smart Queueing functionality.
+     * If SmartQueue is disabled Global Slash/Context Commands get overridden on each {@link DIH4JDA#registerInteractions()} call,
+     * thus, making Global Commands unusable for about an hour, until they're registered again. <br>
+     * By default, this also deletes unknown/unused commands. This behaviour can be disabled with {@link DIH4JDABuilder#disableUnknownCommandDeletion()}.
+     */
+    @Nonnull
+    public DIH4JDABuilder disableSmartQueue() {
+        smartQueuing = false;
+        return this;
+    }
 
-	/**
-	 * Sets the default {@link ExecutableCommand.Type} for all Commands.
-	 *
-	 * @param type The {@link ExecutableCommand.Type}.
-	 */
-	public DIH4JDABuilder setDefaultCommandType(ExecutableCommand.Type type) {
-		DIH4JDA.defaultCommandType = type;
-    return this;
-  }
-  
-  /**
-	 * Disables deletion of unknown/unused commands when using SmartQueue.
-	 */
-	@Nonnull
-	public DIH4JDABuilder disableUnknownCommandDeletion() {
-		deleteUnknownCommands = false;
-		return this;
-	}
+    /**
+     * Sets the default {@link ExecutableCommand.Type} for all Commands.
+     *
+     * @param type The {@link ExecutableCommand.Type}.
+     */
+    public DIH4JDABuilder setDefaultCommandType(ExecutableCommand.Type type) {
+        DIH4JDA.defaultCommandType = type;
+        return this;
+    }
 
-	/**
-	 * Returns a {@link DIH4JDA} instance that has been validated.
-	 *
-	 * @return the built, usable {@link DIH4JDA}
-	 */
-	public DIH4JDA build() throws DIH4JDAException {
-		if (Runtime.getRuntime().availableProcessors() == 1) {
-			DIH4JDALogger.warn("You are running DIH4JDA on a single core CPU. A special system property was set to disable asynchronous command execution.");
-			System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "1");
-		}
-		if (ClasspathHelper.forPackage(commandsPackage).isEmpty()) {
-			throw new InvalidPackageException("Package " + commandsPackage + " does not exist.");
-		}
-		return new DIH4JDA(jda, commandsPackage, registerOnStartup, smartQueuing, deleteUnknownCommands, executor, blockedLogTypes);
-	}
+    /**
+     * Disables deletion of unknown/unused commands when using SmartQueue.
+     */
+    @Nonnull
+    public DIH4JDABuilder disableUnknownCommandDeletion() {
+        deleteUnknownCommands = false;
+        return this;
+    }
+
+    /**
+     * Returns a {@link DIH4JDA} instance that has been validated.
+     *
+     * @return the built, usable {@link DIH4JDA}
+     */
+    public DIH4JDA build() throws DIH4JDAException {
+        if (Runtime.getRuntime().availableProcessors() == 1) {
+            DIH4JDALogger.warn("You are running DIH4JDA on a single core CPU. A special system property was set to disable asynchronous command execution.");
+            System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "1");
+        }
+        if (ClasspathHelper.forPackage(commandsPackage).isEmpty()) {
+            throw new InvalidPackageException("Package " + commandsPackage + " does not exist.");
+        }
+        return new DIH4JDA(jda, commandsPackage, registerOnStartup, smartQueuing, deleteUnknownCommands, executor, blockedLogTypes);
+    }
 }
