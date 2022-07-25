2f98b9b44aee9c567b7df91b925aea2bace74ae9
Dynxsty <jasonlessenich@gmail.com>
2022-07-26 01:21:53 +0200

    re-added Checks#checkEmptyConstructor

--- src/main/java/com/dynxsty/dih4jda/util/Checks.java
+++ src/main/java/com/dynxsty/dih4jda/util/Checks.java
@@ -1,5 +1,10 @@
 package com.dynxsty.dih4jda.util;
 
+import com.dynxsty.dih4jda.DIH4JDALogger;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Utility class for checking certain conditions.
  *
@@ -20,5 +25,20 @@ public class Checks {
 	 */
 	public static boolean checkImplementation(Class<?> base, Class<?> implementation) {
 		return ClassUtils.doesImplement(base, implementation);
+	}
+
+	/**
+	 * Checks if the given class has an empty constructor.
+	 *
+	 * @param base The class to check.
+	 * @return Whether the class has an empty constructor.
+	 * @since v1.5.1
+	 */
+	public static boolean checkEmptyConstructor(@NotNull Class<?> base) {
+		for (Constructor<?> c : base.getConstructors()) {
+			if (c.getParameterCount() == 0) return true;
+		}
+		DIH4JDALogger.warn(String.format("Class %s contains unknown constructor parameters!", base.getSimpleName()));
+		return false;
 	}
 }
