1e7f6e58d7f638a3310f3e15dc32f264eb05e916
-Denux <83671398+DenuxPlays@users.noreply.github.com>
2022-08-06 01:44:18 +0200

    Update src/main/java/com/dynxsty/dih4jda/InteractionHandler.java
    Co-authored-by: Dynxsty <jasonlessenich@gmail.com>

--- src/main/java/com/dynxsty/dih4jda/InteractionHandler.java
+++ src/main/java/com/dynxsty/dih4jda/InteractionHandler.java
@@ -115,10 +115,7 @@ public class InteractionHandler extends ListenerAdapter {
 					DIH4JDALogger.warn("Failed to load requirements in package %s: %s", pkg, e.getCause().getMessage());
 					continue;
 				}
-				String message = e.getMessage();
-				if (message == null) {
-					message = e.getCause().getMessage();
-				}
+				String message = e.getMessage() == null ? e.getCause().getMessage() : e.getMessage();
 				DIH4JDALogger.error("An error occurred while initializing commands in package %s: %s", pkg, message);
 			}
 		}
