8cd23090d75d1a6b7a991585be93c1870379c6e6
Dynxsty <jasonlessenich@gmail.com>
2022-11-13 15:06:22 +0100

    Added actual JUnit tests

--- src/test/java/com/dynxsty/tests/ComponentIdTest.java
+++ src/test/java/com/dynxsty/tests/ComponentIdTest.java
@@ -3,11 +3,13 @@ package com.dynxsty.tests;
 import org.junit.jupiter.api.Test;
 import xyz.dynxsty.dih4jda.util.ComponentIdBuilder;
 
-public class ComponentIdTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ComponentIdTest {
 	@Test
-	public static void main(String[] args) {
-		System.out.println(ComponentIdBuilder.build("component-identifier", "1", 2L, 3.0, '4', 5f));
+	void testComponentIdBuilder() {
+		assertEquals("component-identifier:1:2:3.0:4:5.0", ComponentIdBuilder.build("component-identifier", "1", 2L, 3.0, '4', 5f));
 		ComponentIdBuilder.setDefaultSeparator("!");
-		System.out.println(ComponentIdBuilder.build("component-identifier", "1", 2L, 3.0, '4', 5f));
+		assertEquals("component-identifier!1!2!3.0!4!5.0", ComponentIdBuilder.build("component-identifier", "1", 2L, 3.0, '4', 5f));
 	}
 }
