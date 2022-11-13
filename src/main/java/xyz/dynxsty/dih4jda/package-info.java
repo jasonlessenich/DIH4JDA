f5d6dfff3f75879a04cbe52c5b9592d64d755aa2
-Denux <timon.klinkert@gmail.com>
2022-11-13 22:04:35 +0100

    added firsts `package-info.java`

---
+++ src/main/java/xyz/dynxsty/dih4jda/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Root package of Dynxsty's Interaction Handler for JDA.
+ *
+ * <p>From here you can navigate to the library features.<br>
+ * {@link xyz.dynxsty.dih4jda.DIH4JDABuilder} is probably where you want to start.</p>
+ */
+package xyz.dynxsty.dih4jda;
---
+++ src/main/java/xyz/dynxsty/dih4jda/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package for utility classes that are used directly by {@link xyz.dynxsty.dih4jda.DIH4JDA}, by the user or both.
+ */
+package xyz.dynxsty.dih4jda.util;
