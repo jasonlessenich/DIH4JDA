56e85e2705bfab6759edb78e1e3cd2ca7e286632
-Denux <83671398+DenuxPlays@users.noreply.github.com>
2022-11-13 23:12:29 +0100

    Update src/main/java/xyz/dynxsty/dih4jda/DIH4JDABuilder.java
    Co-authored-by: Dynxsty <jasonlessenich@gmail.com>

--- src/main/java/xyz/dynxsty/dih4jda/DIH4JDABuilder.java
+++ src/main/java/xyz/dynxsty/dih4jda/DIH4JDABuilder.java
@@ -103,7 +103,7 @@ public class DIH4JDABuilder {
 	 * By default, this also deletes unknown/unused commands. This behaviour can be disabled with {@link DIH4JDABuilder#disableUnknownCommandDeletion()}.
 	 *
 	 * @param enable a {@link Boolean} that is true if the {@link SmartQueue} should be enabled for global commands.
-	 * @return the {@link DIH4JDABuilder} instance. Usefully for chaining methods.
+	 * @return The {@link DIH4JDABuilder} for chaining convenience.
 	 */
 	public @Nonnull DIH4JDABuilder setGlobalSmartQueue(boolean enable) {
 		config.setGlobalSmartQueue(enable);
