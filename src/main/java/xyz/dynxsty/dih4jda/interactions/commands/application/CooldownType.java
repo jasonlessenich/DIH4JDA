375cd2a653623efd20fa7aa89f85ca03d28e8796
-Denux <timon.klinkert@gmail.com>
2022-11-15 21:12:43 +0100

    added cooldown types

---
+++ src/main/java/xyz/dynxsty/dih4jda/interactions/commands/application/CooldownType.java
@@ -0,0 +1,23 @@
+package xyz.dynxsty.dih4jda.interactions.commands.application;
+
+/**
+ * The supported {@link xyz.dynxsty.dih4jda.interactions.commands.RestrictedCommand.Cooldown} types.
+ */
+public enum CooldownType {
+    /**
+     * Limits the amount how often a user can execute a command.<br>
+     * <b>User / Global</b>
+     */
+    USER_GLOBAL,
+    /**
+     * Limits the amount how often a user can execute a command on a guild.<br>
+     * <b>User / Guild</b><br>
+     * <b>If no type is configured this will also be the standard type.</b>
+     */
+    USER_GUILD,
+    /**
+     * Limits the amount how often everyone can execute a command on a guild.<br>
+     * <b>everyone / Guild</b>
+     */
+    GUILD
+}
