4856d2f5895d50bbdd807d6818cf6f0b621c0488
-Denux <timon.klinkert@gmail.com>
2022-12-01 20:25:46 +0100

    added java docs

--- src/main/java/xyz/dynxsty/dih4jda/config/DIH4JDAConfig.java
+++ src/main/java/xyz/dynxsty/dih4jda/config/DIH4JDAConfig.java
@@ -13,15 +13,70 @@ import java.util.concurrent.ForkJoinPool;
  */
 @Data
 public class DIH4JDAConfig {
+    /**
+     * The {@link JDA} instance that {@link DIH4JDA} uses.
+     */
     private JDA jda;
+
+    /**
+     * The packages where the commands are located in.<br>
+     * <b>Standard:</b> {@code new String[]{}}
+     */
     private String[] commandsPackages = new String[]{};
+
+    /**
+     * All {@link DIH4JDALogger.Type}s that are not getting logged.<br>
+     * <b>Standard:</b> {@code new DIH4JDALogger.Type[]{}}
+     */
     private DIH4JDALogger.Type[] blockedLogTypes = new DIH4JDALogger.Type[]{};
+
+    /**
+     * A boolean that decides if commands are getting registered on the
+     * {@link net.dv8tion.jda.api.events.session.ReadyEvent}.<br>
+     * <b>Standard:</b> {@code true}
+     */
+
     private boolean registerOnReady = true;
+
+    /**
+     * A boolean that decides if {@link xyz.dynxsty.dih4jda.SmartQueue} should be enabled for the
+     * {@link xyz.dynxsty.dih4jda.interactions.commands.application.RegistrationType#GLOBAL}.<br>
+     * <b>Standard:</b> {@code true}
+     */
     private boolean globalSmartQueue = true;
+
+    /**
+     * A boolean that decides if {@link xyz.dynxsty.dih4jda.SmartQueue} should be enabled for the
+     * {@link xyz.dynxsty.dih4jda.interactions.commands.application.RegistrationType#GUILD}.<br>
+     * <b>Standard:</b> {@code true}
+     */
     private boolean guildSmartQueue = true;
+
+    /**
+     * A boolean that decides if unknown commands should be deleted or not.<br>
+     * <b>Standard:</b> {@code true}
+     */
     private boolean deleteUnknownCommands = true;
+
+    /**
+     * A boolean that decides if a {@link xyz.dynxsty.dih4jda.exceptions.CommandNotRegisteredException} should be
+     * thrown or not.<br>
+     * <b>Standard:</b> {@code true}
+     */
     private boolean throwUnregisteredException = true;
+
+    /**
+     * A boolean that decides if the stacktrace should be printed on an
+     * {@link xyz.dynxsty.dih4jda.events.DIH4JDAThrowableEvent} if no
+     * {@link xyz.dynxsty.dih4jda.events.DIH4JDAEventListener} is registered.
+     * <b>Standard:</b> {@code true}
+     */
     private boolean defaultPrintStacktrace = true;
+
+    /**
+     * The {@link Executor} that will be used to handle the executions of commands.<br>
+     * <b>Standard:</b> {@link ForkJoinPool#commonPool()}
+     */
     private Executor executor = ForkJoinPool.commonPool();
 
     /**
