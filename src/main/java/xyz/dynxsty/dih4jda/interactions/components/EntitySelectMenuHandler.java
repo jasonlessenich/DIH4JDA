8ff1082fb8a55bef6325cc48f44fb11e99347a80
Timon Klinkert <timon.klinkert@denux.dev>
2023-02-14 20:31:29 +0100

    optimized imports

--- src/main/java/xyz/dynxsty/dih4jda/interactions/components/EntitySelectMenuHandler.java
+++ src/main/java/xyz/dynxsty/dih4jda/interactions/components/EntitySelectMenuHandler.java
@@ -2,7 +2,6 @@ package xyz.dynxsty.dih4jda.interactions.components;
 
 import net.dv8tion.jda.api.entities.IMentionable;
 import net.dv8tion.jda.api.events.interaction.component.EntitySelectInteractionEvent;
-import xyz.dynxsty.dih4jda.DIH4JDA;
 import xyz.dynxsty.dih4jda.util.ComponentIdBuilder;
 
 import javax.annotation.Nonnull;
