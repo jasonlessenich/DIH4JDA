4e218c7b92c60b38875c61203a2da9f16da6af62
DynxstyGIT <jasonlessenich@gmail.com>
2023-06-14 18:20:15 +0200

    Added `TextCommandData`

--- src/main/java/xyz/dynxsty/dih4jda/interactions/commands/text/TextCommand.java
+++ src/main/java/xyz/dynxsty/dih4jda/interactions/commands/text/TextCommand.java
@@ -4,62 +4,14 @@ import xyz.dynxsty.dih4jda.events.text.TextCommandEvent;
 import xyz.dynxsty.dih4jda.interactions.commands.ExecutableCommand;
 import xyz.dynxsty.dih4jda.interactions.commands.RestrictedCommand;
 
-import java.util.List;
-
 public abstract class TextCommand extends RestrictedCommand implements ExecutableCommand<TextCommandEvent> {
-	private String name;
-	private String description;
-	private String category;
-	private String[] aliases = new String[]{};
-	private TextOptionData[] options = new TextOptionData[]{};
+	private TextCommandData commandData;
 
-	// TODO: Docs
-	public String getName() {
-		return name;
+	public void setCommandData(TextCommandData commandData) {
+		this.commandData = commandData;
 	}
 
-	// TODO: Docs
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	// TODO: Docs
-	public String getDescription() {
-		return description;
-	}
-
-	// TODO: Docs
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	// TODO: Docs
-	public String getCategory() {
-		return category;
-	}
-
-	// TODO: Docs
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	// TODO: Docs
-	public void setAliases(String... aliases) {
-		this.aliases = aliases;
-	}
-
-	// TODO: Docs
-	public String[] getAliases() {
-		return aliases;
-	}
-
-	// TODO: Docs
-	public void setOptions(TextOptionData... options) {
-		this.options = options;
-	}
-
-	// TODO: Docs
-	public List<TextOptionData> getOptions() {
-		return List.of(options);
+	public TextCommandData getCommandData() {
+		return commandData;
 	}
 }
